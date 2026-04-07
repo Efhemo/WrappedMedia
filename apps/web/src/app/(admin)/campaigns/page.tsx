f0a9c651b874c552a0bd4511e7f3885741ac537c
Femi Adegbite <femiadegbite@Femis-MacBook-Pro.local>
2026-04-06 22:23:43 -0600

    fixed admin can access resources

--- apps/web/src/app/(admin)/campaigns/page.tsx
+++ apps/web/src/app/(admin)/campaigns/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '../../../lib/supabase'
+import { supabaseAdmin as supabase } from '../../../lib/supabase-admin'
 import { CampaignForm } from './campaign-form'
 
 async function getCampaigns() {
